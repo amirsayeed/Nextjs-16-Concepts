b9f61a21e5fd6f287cf31ce9fcac0812fba02d15
amirsayeed <amirsayeed090@gmail.com>
2026-08-03 01:17:48 +0600

    search input implemented

--- Core-concepts-2/src/app/foods/page.jsx
+++ Core-concepts-2/src/app/foods/page.jsx
@@ -1,20 +1,27 @@
 import FoodCard from '@/components/card/FoodCard';
+import InputSearch from '@/components/InputSearch';
 import React from 'react';
 
-const getFoods = async() =>{
-    const res = await fetch(` https://taxi-kitchen-api.vercel.app/api/v1/foods/random`)
+const getFoods = async(search) =>{
+    const res = await fetch(`https://taxi-kitchen-api.vercel.app/api/v1/foods/random?search=${search}`);
     const data = await res.json();
-    return data.foods;
+    return data.foods || [];
 }
 
-const FoodsPage = async () => {
-    const foods = await getFoods();
+const FoodsPage = async ({searchParams}) => {
+    const {search = " "} = await searchParams;
+    const foods = await getFoods(search);
     // console.log(foods);
     return (
         <div>
            Total Foods Found: {foods.length}
-           <div className="grid grid-cols-3 gap-4">
+           <div className='my-4'>
+            <InputSearch/>
+           </div>
+           <div className="">
+            <div className="grid grid-cols-3 gap-4">
             {foods.map((food) => <FoodCard key={food.id} food={food}></FoodCard>)}
+           </div>
            </div>
         </div>
     );
---
+++ Core-concepts-2/src/components/InputSearch.jsx
@@ -0,0 +1,29 @@
+'use client'
+import { useRouter, useSearchParams } from 'next/navigation';
+import React from 'react'
+
+const InputSearch = () => {
+const router = useRouter();
+const params = useSearchParams();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const searchValue = e.target.search.value;
+  const newParams = new URLSearchParams(params.toString());
+  newParams.set('search', searchValue);
+    router.push(`?${newParams.toString()}`)
+}
+
+  return (
+    <div>
+      <form onSubmit = {handleSubmit}>
+        <input name="search" type="text"
+        className='px-1 py-3 border-2 w-2xl' 
+        placeholder="Enter Food Name" />
+        <button type="submit" className='bg-blue-500 text-white px-1 py-3 rounded'>Search</button>
+      </form>
+    </div>
+  )
+}
+
+export default InputSearch
