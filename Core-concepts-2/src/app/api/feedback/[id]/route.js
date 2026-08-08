29ae50529caeb39b1aa2f25c7b3388b385919257
amirsayeed <amirsayeed090@gmail.com>
2026-08-08 12:57:41 +0600

    CRUD feedback with MongoDB

--- Core-concepts-2/src/app/api/feedback/[id]/route.js
+++ Core-concepts-2/src/app/api/feedback/[id]/route.js
@@ -1,11 +1,29 @@
-import { feedback } from "../../route";
+import { connect } from "@/app/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+const feedbackCollection = connect("feedbacks");
 
 export async function GET(request,{params}) {
     const {id} = await params;
-    const feedbackItem = feedback.find(item => item.id == id) || {};
+    const query = {_id: new ObjectId(id)};
+    const result = await feedbackCollection.findOne(query) || {};
 
-    return Response.json({
-        status: 200,
-        data: feedbackItem
-    })
+    return Response.json(result);
+}
+
+export async function DELETE(request,{params}) {
+    const {id} = await params;
+    const query = {_id: new ObjectId(id)};
+    const result = await feedbackCollection.deleteOne(query) || {};
+
+    return Response.json(result);
+}
+
+export async function PATCH(request,{params}) {
+    const {id} = await params;
+    const {message} = await request.json();
+    const query = {_id: new ObjectId(id)};
+    const result = await feedbackCollection.updateOne(query, {$set: {message}}) || {};
+
+    return Response.json(result);
 }
