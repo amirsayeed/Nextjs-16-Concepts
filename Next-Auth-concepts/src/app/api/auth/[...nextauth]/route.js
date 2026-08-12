76ab52e82788d535f487499d243fb072c17880e9
amirsayeed <amirsayeed090@gmail.com>
2026-08-12 17:29:12 +0600

    login with DB implemented

--- Next-Auth-concepts/src/app/api/auth/[...nextauth]/route.js
+++ Next-Auth-concepts/src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,13 @@
+import { dbConnect } from "@/lib/dbConnect";
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from 'bcryptjs';
 
-const userList = [
-  {name: "hablu", password: "1234"},
-  {name: "dablu", password: "2537"},
-  {name: "bablu", password: "9420"}
-]
+// const userList = [
+//   {name: "hablu", password: "1234"},
+//   {name: "dablu", password: "2537"},
+//   {name: "bablu", password: "9420"}
+// ]
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -13,17 +15,18 @@ export const authOptions = {
     CredentialsProvider({
        name: 'Email & Password',
         credentials: {
-          username: { label: "Username", type: "text", placeholder: "jsmith" },
-          password: { label: "Password", type: "password" },
-          secretCode : {label: "Secret Code", type: "number", placeholder: "enter code"}
+          email: { label: "Email", type: "email", placeholder: "Enter your email" },
+          password: { label: "Password", type: "password", placeholder: "Enter your password" },
         },
         async authorize(credentials, req) {
-          const {username, password, secretCode} = credentials;
+          const {email, password} = credentials;
           // Return null if user data could not be retrieved
-          const user = userList.find((u) => u.name == username);
+          // const user = userList.find((u) => u.name == username);
+
+          const user = await dbConnect("users").findOne({email});
           if(!user) return null;
           
-          const isPasswordOk = user.password == password;
+          const isPasswordOk = await bcrypt.compare(password, user?.password);
           
           if(isPasswordOk){
             return user;
