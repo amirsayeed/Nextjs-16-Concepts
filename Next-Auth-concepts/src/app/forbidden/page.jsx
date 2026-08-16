960bc58038e1843a36abe547c96ec56be545a9b6
amirsayeed <amirsayeed090@gmail.com>
2026-08-16 17:34:21 +0600

    admin routes added

---
+++ Next-Auth-concepts/src/app/forbidden/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Forbidden = () => {
+    return (
+        <div className='py-20 text-6xl text-center'>
+           Forbidden 
+        </div>
+    );
+};
+
+export default Forbidden;
--- Next-Auth-concepts/src/proxy.js
+++ Next-Auth-concepts/src/proxy.js
@@ -1,7 +1,7 @@
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server'
 const privateRoutes = ["/private", "/dashboard", "/secret"];
-
+const adminRoutes = ["/dashboard"];
 // This function can be marked `async` if using `await` inside
 export async function proxy(req) {
 //   return NextResponse.redirect(new URL('/home', request.url))
@@ -9,13 +9,20 @@ const token = await getToken({req});
 const reqPath = req.nextUrl.pathname; 
 const isAuthenticated = Boolean(token);
 const isUser = token?.role === "user";
+const isAdmin = token?.role === "admin";
 const isPrivate = privateRoutes.some((route)=> reqPath.startsWith(route));
+const isAdminRoute = adminRoutes.some((route) => reqPath.startsWith(route));
 
-if(!isAuthenticated && isPrivate){
-    
+// logic for private route only
+if(!isAuthenticated && isPrivate){ 
     const loginUrl = new URL("/api/auth/signin", req.url)
     loginUrl.searchParams.set("callbackUrl", reqPath);
     return NextResponse.redirect(loginUrl);
+}
+
+// logic for admin route only
+if(isAuthenticated && !isAdmin && isAdminRoute){
+    return NextResponse.redirect(new URL("/forbidden", req.url));
 }
 console.log({isAuthenticated, isUser, reqPath});
 
